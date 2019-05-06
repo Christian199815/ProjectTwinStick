5f41b0a13df9aaf09985ba4be11bfcd79566bc43
Aaron Moulding <529210@student.glu.nl>
2019-05-06 14:27:17 +0200

    Player

---
+++ TwinStickShooter/Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    [Header("Bullet settings")]
+    public float bulletSpeed = 7.0f;
+
+    private void Update () {
+		transform.position += transform.forward * bulletSpeed;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+
+        if(other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
